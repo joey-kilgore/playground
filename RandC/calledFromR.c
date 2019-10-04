5794fa38e270a5065a87a95ed34fd10c133a44a5
joey-kilgore <joey.a.kilgore@gmail.com>
2019-10-03 22:59:15 -0400

    Added command line options for calledFromR.c R (within the callingCfromR.r program) can pass optional parameters to the executable to give power over the simulation in R, and then run the simulation in C, and bring the data back into R

--- RandC/calledFromR.c
+++ RandC/calledFromR.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    printf("Hello from C\n");
+// calledFromR.c takes in a number from as an argument and will
+//  generate data.csv with multiples of that number
+int main(int argc, char **argv){
+    printf("Hello from C: %s\n",argv[0]);
     FILE *fp;
     fp = fopen("data.csv","w+");
-    fprintf(fp, "1,2,3,4\n");
+    if(argc==2){
+        int num = atoi(argv[1]);
+        fprintf(fp, "%d,%d,%d,%d\n",num,num*2,num*3,num*4);
+        fprintf(fp, "%d,%d,%d,%d\n",num,num*2,num*3,num*4);
+    }
+    else if(argc==3){
+        int num = atoi(argv[1]);
+        int num2 = atoi(argv[2]);
+        fprintf(fp, "%d,%d,%d,%d\n",num,num*2,num*3,num*4);
+        fprintf(fp, "%d,%d,%d,%d\n",num2,num2*2,num2*3,num2*4);
+    }
+    else{
+        fprintf(fp, "1,2,3,4\n");
+        fprintf(fp, "1,2,3,4\n");
+    }
     fclose(fp);
     return 0;
 }
--- RandC/callingCfromR.r
+++ RandC/callingCfromR.r
@@ -1,6 +1,19 @@
+# callingCfromR.r is a sample of how an R program can use the executables from
+#   a compiled C/C++ program and run them to generate data, then bring the data
+#   back into R to be analyzed
+
 print("Hello from R")
-system("./calledFromR.exe")
+
+# This is where the magic happens, essentially we simply send the bash command
+#   to run the executable. By passing different arguments we can control the simulation
+
+# system("./calledFromR.exe")
+# system("./calledFromR.exe 2")
+system("./calledFromR.exe 2 3")
+
+# Gather the data generated from the executable
 data <- read.csv(file = "data.csv", header=FALSE, sep=",")
 x <- data[1,]
-print(data)
+y <- data[2,]
 print(x)
+print(y)
