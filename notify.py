04bcbab036054b933a66a8567d6ed551dfa390c2
Joey Kilgore <joey.a.kilgore@gmail.com>
2024-08-05 03:37:09 -0400

    notify example code

---
+++ notify.py
@@ -0,0 +1,7 @@
+
+# DON'T FORGET TO RUN THIS FIRST ON NEW SYSTEM!
+# notify-run register
+
+from notify_run import Notify
+notify = Notify(endpoint='https://notify.run/LfWJXqiVejLyBTWltqR4')
+notify.send('Test notification hopefully fixed again do not disturb!')
